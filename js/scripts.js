166da53d82e244cd164abeb2544e119586b1c0e2
Rithaven <veneruth4@gmail.com>
2019-07-26 17:34:21 +0200

    those

---
+++ js/scripts.js
@@ -0,0 +1,49 @@
+$(document).ready(function(){
+$("form").submit(function(event){
+var a = $("#name").val();
+alert(a+ "we have received your message. Thank you fro reaching out to us.");
+});
+});
+
+$(document).ready(function(){
+$('#design').click(function(){
+$('#desig').toggle();
+$('#design').toggle();
+});
+$('#desig').click(function(){
+  $('#design').toggle();
+  $('#desig').toggle();
+});
+
+$('#icon').click(function(){
+  $('#con').toggle();
+  $('#icon').toggle();
+  });
+  $('#con').click(function(){
+    $('#icon').toggle();
+    $('#con').toggle();
+  });
+
+  $('#product').click(function(){
+    $('#produc').toggle();
+    $('#product').toggle();
+    });
+    $('#produc').click(function(){
+      $('#product').toggle();
+      $('#produc').toggle();
+    });
+});
+
+$(document).ready(function(){
+  $('#one').hover(function(){
+    $('#pp').show();
+    // $('#pp').fadeOut();
+  });
+});
+// $(document).ready(function(){
+//   $("form").submit(function(event){
+//     var hee = $("name").val();
+//     alert(hee+ " ");
+
+// });
+// });
